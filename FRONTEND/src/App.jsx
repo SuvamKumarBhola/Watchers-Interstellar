e8b3aadd34a8b62ec448fea8f023e060fe6d72b9
Sayed Ahemmad <143632368+Sayeed-5@users.noreply.github.com>
2026-02-07 14:11:30 +0530

    added my frontend code

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,26 @@
+import Footer from "./components/footer";
+import LenisScroll from "./components/lenis-scroll";
+import Navbar from "./components/navbar";
+import Features from "./sections/why-cosmic-watch";
+import HeroSection from "./sections/hero-section";
+import Carousul from "./sections/feature-preview";
+import { Routes, Route } from "react-router-dom";
+
+export default function Page() {
+    return (
+        <>
+            <LenisScroll />
+            <Navbar />
+            <main className="px-6 md:px-16 lg:px-24 xl:px-32">
+                <HeroSection />
+                <Carousul />
+                <Features />
+            </main>
+            {/* <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes> */}
+            <Footer />
+        </>
+    );
+}
---
+++ FRONTEND/src/sections/why-cosmic-watch.jsx
@@ -0,0 +1,42 @@
+import SectionTitle from "../components/section-title";
+import { motion } from "framer-motion";
+
+export default function Features() {
+    const sectionData = [
+        {
+            title: "Live NEO Monitoring",
+            description: "Fetches real-time asteroid data from NASA NeoWs API and displays it in an intuitive dashboard.",
+            className: "py-10 border-b border-slate-700 md:py-0 md:border-r md:border-b-0 md:px-10"
+        },
+        {
+            title: "Risk Analysis Engine",
+            description: "Calculates asteroid threat level using size, velocity, and miss distance, providing actionable insights.",
+            className: "py-10 border-b border-slate-700 md:py-0 lg:border-r md:border-b-0 md:px-10"
+        },
+        {
+            title: "Custom Alerts",
+            description: "Get notified before dangerous close-approach events based on user-defined thresholds for size and proximity.",
+            className: "py-10 border-b border-slate-700 md:py-0 md:border-b-0 md:px-10"
+        },
+    ];
+    return (
+        <section className="flex flex-col items-center" id="about">
+            <SectionTitle title="Our Features"/>
+            <div className="relative max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-8 md:px-0 mt-18">
+                {sectionData.map((data, index) => (
+                    <motion.div key={data.title} className={data.className}
+                        initial={{ y: 150, opacity: 0 }}
+                        whileInView={{ y: 0, opacity: 1 }}
+                        viewport={{ once: true }}
+                        transition={{ delay: `${index * 0.15}`, type: "spring", stiffness: 320, damping: 70, mass: 1 }}
+                    >
+                        <div className="mt-5 space-y-2">
+                            <h3 className="text-base font-medium text-slate-200">{data.title}</h3>
+                            <p className="text-sm text-slate-400">{data.description}</p>
+                        </div>
+                    </motion.div>
+                ))}
+            </div>
+        </section>
+    );
+}
